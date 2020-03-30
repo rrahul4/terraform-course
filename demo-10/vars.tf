31ac10a9ab9a331a1a9db5463cd4789d330779bf
Rahulkumar Rakhonde <rahul.rakhonde@gmail.com>
2020-03-30 20:19:44 +0530

    Modifying Files for Demo

--- demo-10/vars.tf
+++ demo-10/vars.tf
@@ -1,5 +1,5 @@
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-east-1"
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
@@ -13,7 +13,7 @@ variable "PATH_TO_PUBLIC_KEY" {
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
+    us-east-1 = "ami-0e2ff28bfb72a4e45"
     us-west-2 = "ami-06b94666"
     eu-west-1 = "ami-844e0bf7"
   }
